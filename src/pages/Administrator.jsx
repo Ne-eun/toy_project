8843bdb06f5df886285e47d3bdc697cd3f4afc4f
ne-eun <p.tyche92@gmail.com>
2020-12-02 20:02:46 +0900

    어드민 페이지 구성

---
+++ src/pages/Administrator.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import LeftNav from '../components/Left_Nav.jsx'
+import RightContents from '../router/Right_Contents.jsx'
+
+
+function Administrator() {
+	return(
+		<>
+			<LeftNav />
+			<RightContents />
+		</>
+	)
+}
+
+export default Administrator;
