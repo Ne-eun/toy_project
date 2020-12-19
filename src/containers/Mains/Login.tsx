572325f17aee27505daf3b8cf8f9e5a590b2a48d
ne-eun <p.ryche92@gmail.com>
2020-12-19 14:23:48 +0900

    로그인 기능구현

--- src/containers/Mains/Login.tsx
+++ src/containers/Mains/Login.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import styled from 'styled-components';
 import Button from '../../components/Atoms/AtomButton';
 import Title from '../../components/Atoms/AtomTitle';
 import GoPage from '../../components/Atoms/AtomGoPage';
 import LayoutFlex from '../../components/Atoms/AtomLayoutFlex';
 import InputSet from '../../components/Atoms/AtomInputSet';
+import api from '../../router/api';
+import { useHistory } from 'react-router-dom';
 
 export const LoginBoxStyle = styled.div`
   width: 100px;
@@ -21,21 +23,60 @@ export const LoginBoxStyle = styled.div`
 `;
 
 function Login() {
+  let emailDom = useRef<HTMLInputElement>();
+  let passwordDom = useRef<HTMLInputElement>();
+  let history = useHistory();
+
+  const validationHandle = () => {
+    if (emailDom.current.value === '') {
+      alert('이메일을 입력하세요');
+    } else if (passwordDom.current.value === '') {
+      alert('비밀번호를 입력하세요');
+    } else {
+      loginRequest();
+    }
+  };
+  const loginRequest = () => {
+    api
+      .post('/user/login', {
+        email: emailDom.current.value,
+        password: passwordDom.current.value,
+      })
+      .then((res: object) => {
+        if (res.data.status === 1010) {
+          alert('가입된 이메일이 없습니다');
+        } else if (res.data.status === 1011) {
+          alert('비밀번호가 일치하지 않습니다');
+        } else if (res.data.status === 200) {
+          console.log(res.data.data);
+          localStorage.setItem('AUTH_TOKEN', res.data.data.token);
+          history.push('/admin');
+        }
+      })
+      .catch((error: any) => console.log(error));
+  };
   return (
     <LoginBoxStyle>
       <Title className="bold">시작하기</Title>
       <Title className="sub mt_8">환영합니다! 계정이 없다면 회원가입 후 이용해주세요.</Title>
 
       <div className="form_box">
-        <InputSet label="이메일 주소" name="email" type="email" placeholder="이메일을 입력하세요" />
         <InputSet
+          useref={emailDom}
+          label="이메일 주소"
+          name="email"
+          type="email"
+          placeholder="이메일을 입력하세요"
+        />
+        <InputSet
+          useref={passwordDom}
           label="비밀번호"
           name="password"
           type="password"
           placeholder="비밀번호를 입력하세요"
         />
       </div>
-      <Button>로그인</Button>
+      <Button onClick={validationHandle}>로그인</Button>
       <LayoutFlex className="justify_btw mt_20">
         <GoPage to="#" title="비밀번호 찾기" />
         <GoPage to="register" title="가입하기" className="underline" />
